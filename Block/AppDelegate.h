fdbca04a9da7f3b15caf2d0caf96b633cebbe9f0
王跃龙 <wangylw@yonyou.com>
2016-09-06 15:17:23 +0800

    Initial Commit

---
+++ Block/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Block
+//
+//  Created by wangylw on 16/9/6.
+//  Copyright © 2016年 wangyl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Block/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Block
+//
+//  Created by wangylw on 16/9/6.
+//  Copyright © 2016年 wangyl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
